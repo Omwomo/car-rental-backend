604ba1df91b80465cf2ec87d2c48bbfbe1f30d5d
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-16 17:37:19 +0530

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :reservations, foreign_key: 'customer_id'
-    has_many :items, foreign_key: 'admin_id'
+  has_many :reservations, foreign_key: 'customer_id'
+  has_many :items, foreign_key: 'admin_id'
 end
