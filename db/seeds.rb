3a3c8ef5515f81401fc41a4c265d3374762a50b0
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-16 17:36:03 +0530

    Add associations for user and reservation model and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create Users
+user1 = User.create(username: 'jdoe123', first_name: 'John', last_name: 'Doe', city: 'Miami')
+user2 = User.create(username: 'janesmith89', first_name: 'Jane', last_name: 'Smith', city: 'San Francisco')
+
+# Create Items
+item1 = Item.create(admin: user1, name: 'Item 1', image: 'image_data_1', description: 'Description 1', city: 'Chicago')
+item2 = Item.create(admin: user1, name: 'Item 2', image: 'image_data_2', description: 'Description 2', city: 'Los Angeles')
+item3 = Item.create(admin: user2, name: 'Item 3', image: 'image_data_3', description: 'Description 3', city: 'Seattle')
+
+# Create Reservations
+reservation1 = Reservation.create(customer: user2, reserve_for_use_date: Date.today, city: 'San Francisco')
+reservation2 = Reservation.create(customer: user1, reserve_for_use_date: Date.tomorrow, city: 'Miami')
+
+# Create ReservationItems
+ReservationItem.create(reservation: reservation1, item: item1)
+ReservationItem.create(reservation: reservation1, item: item2)
+ReservationItem.create(reservation: reservation2, item: item3)
