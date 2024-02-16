69508b279d80ff5da12a60ff2ffe9145b479353b
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-16 17:37:01 +0530

    seed data is being add properlu

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,4 @@ reservation2 = Reservation.create(customer: user1, reserve_for_use_date: Date.to
 ReservationItem.create(reservation: reservation1, item: item1)
 ReservationItem.create(reservation: reservation1, item: item2)
 ReservationItem.create(reservation: reservation2, item: item3)
+puts "seeds data created successfully"
