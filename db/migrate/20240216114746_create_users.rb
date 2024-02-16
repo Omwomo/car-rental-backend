e58f4584bda487c3e93738e4f4f6d0b73bd48e57
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-16 17:18:51 +0530

    add users migration

---
+++ db/migrate/20240216114746_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :city
+
+      t.timestamps
+    end
+    add_index :users, :username
+  end
+end
