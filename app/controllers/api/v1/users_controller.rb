d6a5673b33ea4979ebc2ad8ded8fc93f2c3a7bde
Omwomo <eugeneoduor79@gmail.com>
2024-02-17 08:56:39 +0300

    Implemented show action to display the selected user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,3 +10,21 @@ class Api::V1::UsersController < ApplicationController
       data: @users.map { |user| UserSerializer.new(user).serializable_hash[:data][:attributes] }
     }, status: :ok
   end
+
+  # GET /api/v1/users/:username
+  def show
+    @user = User.find_by(username: params[:username])
+
+    if @user
+      render json: {
+        status: { code: 200, message: 'User information retrieved successfully.' },
+        data: UserSerializer.new(@user).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+        status: 404,
+        message: 'User not found.'
+      }, status: :not_found
+    end
+  end
+end
