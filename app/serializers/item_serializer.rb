406cd34795433391d651e62f5f2456ae48867178
Omwomo <eugeneoduor79@gmail.com>
2024-02-17 10:11:15 +0300

    Included serializer for item

---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,4 @@
+class ItemSerializer
+  include JSONAPI::Serializer
+  attributes :name, :description, :image, :city, :rates_per_day, :rates_per_hour, :availability_status, :collision_cover, :mileage, :fuel_type
+end
