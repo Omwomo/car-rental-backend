4469c226e740c0a2e3f11798b3f579665a6a09f8
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-20 21:32:39 +0530

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
   has_many :items, foreign_key: 'admin_id'
 
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-         
+
   validates :username, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :first_name, :last_name, presence: true, length: { maximum: 50 }
   validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
