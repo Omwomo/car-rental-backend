154e16090ea3d1955f7eb005f7acf4bf91a0cd81
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-20 17:57:16 +0530

    configure database and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,24 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.new(
-  email:  's@g.c', #ENV['SUPER_ADMIN_EMAIL'],
-  username: 's_admin', #ENV['SUPER_ADMIN_USERNAME'],
-  first_name: 'Stanley', #ENV['SUPER_ADMIN_FIRST_NAME'],
-  last_name: 'Osagie' #ENV['SUPER_ADMIN_LAST_NAME']
-)
+# Create admin user
+admin = User.create(username: 'admin', first_name: 'Admin', last_name: 'User', city: 'AnyCity', email: 'admin@example.com', password: 'password')
 
-user.password = '123456' #ENV['SUPER_ADMIN_PASSWORD']
-user.password_confirmation = '123456' #ENV['SUPER_ADMIN_PASSWORD']
-user.save
+# Create regular users
+user1 = User.create(username: 'john_doe', first_name: 'John', last_name: 'Doe', city: 'New York')
+user2 = User.create(username: 'jane_smith', first_name: 'Jane', last_name: 'Smith', city: 'Los Angeles')
+
+# Create Items
+item1 = Item.create(admin: admin, name: 'Item 1', image: 'image_data_1', description: 'Description 1', city: 'Chicago', admin_id: '1')
+item2 = Item.create(admin: admin, name: 'Item 2', image: 'image_data_2', description: 'Description 2', city: 'San Francisco', admin_id: '1')
+item3 = Item.create(admin: admin, name: 'Item 3', image: 'image_data_3', description: 'Description 3', city: 'Seattle', admin_id: '1')
+
+# Create Reservations
+reservation1 = Reservation.create(customer: user2, reserve_for_use_date: Date.today, city: 'Los Angeles', customer_id: '1')
+reservation2 = Reservation.create(customer: user1, reserve_for_use_date: Date.tomorrow, city: 'New York', customer_id: '1')
+
+# Create ReservationItems
+ReservationItem.create(reservation: reservation1, item: item1)
+ReservationItem.create(reservation: reservation1, item: item2)
+ReservationItem.create(reservation: reservation2, item: item3)
+puts 'data added success'
