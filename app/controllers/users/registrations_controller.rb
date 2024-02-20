eae1fe50677947685cc14986a828a123b278785d
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-20 17:34:25 +0530

    configure controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  include RackSessionFix
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
   respond_to :json
 
-  private
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name city email])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name city])
+  end
 
   def respond_with(resource, _opts = {})
     if request.method == 'POST' && resource.persisted?
@@ -16,7 +26,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
       }, status: :ok
     else
       render json: {
-        status: { code: 422, message: "User not created. #{resource.errors.full_messages.to_sentence}" }
+        status: {
+          code: 422,
+          message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"
+        }
       }, status: :unprocessable_entity
     end
   end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,32 @@
 class Users::SessionsController < Devise::SessionsController
-  include RackSessionFix
   respond_to :json
 
-  private
+  def create
+    login = params[:user][:login]
+    password = params[:user][:password]
+
+    if login.present?
+      it_is_email = login.match?(Devise.email_regexp)
+      authentication_key = it_is_email ? :email : :username
+
+      resource = User.find_for_database_authentication({ authentication_key => login&.downcase })
+
+      if resource&.valid_password?(password)
+        sign_in(resource_name, resource)
+        respond_with(resource)
+      else
+        render json: { error: 'Invalid login credentials' }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Login (email or username) is required' }, status: :unprocessable_entity
+    end
+  end
+
+  protected
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: { code: 200, message: 'Logged in sucessfully.' },
+      status: { code: 200, message: 'Logged in sucessfully.', current_user: current_user.username },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
   end
