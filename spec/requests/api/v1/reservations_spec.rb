36b90f55fc1cc8a59f373cebf0a6d1b1b26b317d
Omwomo <eugeneoduor79@gmail.com>
2024-02-26 17:01:32 +0300

    Create intergration spec file for reservation

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -1,66 +1,95 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/v1/reservations', type: :request do
+RSpec.describe 'Api::V1::Reservations', type: :request do
+  # rubocop:disable Metrics/BlockLength
   path '/api/v1/reservations' do
     get('list reservations') do
-      response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      tags 'Reservations'
+      produces 'application/json'
+      response(200, 'All reservations fetched') do
+        description 'All reservations fetched successfully'
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   date: { type: :string, format: 'date' },
+                   city: { type: :string },
+                   user_id: { type: :integer },
+                   item_id: { type: :integer }
+                 },
+                 required: %w[id date city user_id item_id]
+               }
+
+        let(:id) { Reservation.create(date: '2024-02-28', city: 'City1', user_id: 1, item_id: 1).id }
         run_test!
       end
     end
 
     post('create reservation') do
-      response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      tags 'Reservations'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          date: { type: :string, format: 'date' },
+          city: { type: :string },
+          user_id: { type: :integer },
+          item_id: { type: :integer }
+        },
+        required: %w[date city user_id item_id]
+      }
+
+      response(201, 'Reservation created') do
+        let(:reservation) { { date: '2024-02-28', city: 'City1', user_id: 1, item_id: 1 } }
+        run_test!
+      end
+
+      response(422, 'Invalid request') do
+        let(:reservation) { { date: '2024-02-28', city: 'City1', user_id: 1 } }
         run_test!
       end
     end
   end
 
   path '/api/v1/reservations/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show reservation') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+      tags 'Reservations'
+      produces 'application/json'
+      response(200, 'Reservation fetched') do
+        description 'All reservations fetched successfully'
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   date: { type: :string, format: 'date' },
+                   city: { type: :string },
+                   user_id: { type: :integer },
+                   item_id: { type: :integer }
+                 },
+                 required: %w[id date city user_id item_id]
+               }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        let(:id) { Reservation.create(date: '2024-02-28', city: 'City1', user_id: 1, item_id: 1).id }
+        run_test!
+      end
+
+      response(404, 'Reservation not found') do
+        let(:id) { 'invalid' }
         run_test!
       end
     end
 
     delete('delete reservation') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      tags 'Reservations'
+      response(200, 'Reservation destroyed') do
+        let(:id) { Reservation.create(date: '2024-02-28', city: 'City1', user_id: 1, item_id: 1).id }
         run_test!
       end
     end
   end
+  # rubocop:enable Metrics/BlockLength
 end
