8ccffbcdd07f60a0d9ece576149290fa36b92699
Omwomo <eugeneoduor79@gmail.com>
2024-02-26 16:45:43 +0300

    created documentation for the Item

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,66 +1,140 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/v1/items', type: :request do
+RSpec.describe 'Api::V1::Items', type: :request do
+  # rubocop:disable Metrics/BlockLength
   path '/api/v1/items' do
     get('list items') do
-      response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      tags 'Items'
+      produces 'application/json'
+      response(200, 'All items fetched') do
+        description 'All items fetched successfully'
+        schema type: :object,
+               properties: {
+                 message: { type: :array, items: { type: :string } },
+                 data: {
+                   type: :array,
+                   items: {
+                     type: :object,
+                     properties: {
+                       id: { type: :string },
+                       name: { type: :string },
+                       description: { type: :string },
+                       image: { type: :string },
+                       city: { type: :string },
+                       financeFee: { type: :integer },
+                       totalAmount: { type: :integer },
+                       duration: { type: :integer },
+                       apr: { type: :float },
+                       purchaseFee: { type: :float },
+                       adminId: { type: :integer }
+                     },
+                     required: %w[id name description image city financeFee totalAmount duration
+                                  apr purchaseFee adminId]
+                   }
+                 }
+               }
+
+        let(:id) do
+          Item.create(name: 'item1', description: 'description1', image: 'image1.jpg', city: 'City1', financeFee: 100,
+                      totalAmount: 500, duration: 30, apr: 5.0, purchaseFee: 10.0, adminId: 1).id
         end
         run_test!
       end
     end
 
     post('create item') do
+      tags 'Items'
+      consumes 'application/json'
+      parameter name: :item, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          description: { type: :string },
+          image: { type: :string },
+          city: { type: :string },
+          financeFee: { type: :integer },
+          totalAmount: { type: :integer },
+          duration: { type: :integer },
+          apr: { type: :float },
+          purchaseFee: { type: :float },
+          adminId: { type: :integer }
+        },
+        required: %w[name description image city financeFee totalAmount duration apr
+                     purchaseFee adminId]
+      }
+
       response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        let(:item) do
+          { name: 'foo', description: 'bar', image: 'image.jpg', city: 'City', financeFee: 100, totalAmount: 500,
+            duration: 30, apr: 5.0, purchaseFee: 10.0, adminId: 1 }
         end
+        run_test!
+      end
+
+      response(422, 'invalid request') do
+        let(:item) { { name: 'foo' } }
         run_test!
       end
     end
   end
 
   path '/api/v1/items/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show item') do
+      tags 'Items'
+      produces 'application/json'
       response(200, 'successful') do
-        let(:id) { '123' }
+        description 'car retrieved succesfully'
+        schema type: :object,
+               properties: {
+                 message: { type: :array, items: { type: :string } },
+                 data: {
+                   type: :array,
+                   items: {
+                     type: :object,
+                     properties: {
+                       id: { type: :string },
+                       name: { type: :string },
+                       description: { type: :string },
+                       image: { type: :string },
+                       city: { type: :string },
+                       financeFee: { type: :integer },
+                       totalAmount: { type: :integer },
+                       duration: { type: :integer },
+                       apr: { type: :float },
+                       purchaseFee: { type: :float },
+                       adminId: { type: :integer }
+                     },
+                     required: %w[id name description image city financeFee totalAmount duration
+                                  apr purchaseFee adminId]
+                   }
+                 }
+               }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        let(:id) do
+          Item.create(name: 'foo', description: 'bar', image: 'image.jpg', city: 'City', financeFee: 100,
+                      totalAmount: 500, duration: 30, apr: 5.0, purchaseFee: 10.0, adminId: 1).id
         end
+        run_test!
+      end
+
+      response(404, 'item not found') do
+        let(:id) { 'invalid' }
         run_test!
       end
     end
 
     delete('delete item') do
+      tags 'Items'
       response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        let(:id) do
+          Item.create(name: 'foo', description: 'bar', image: 'image.jpg', city: 'City', financeFee: 100,
+                      totalAmount: 500, duration: 30, apr: 5.0, purchaseFee: 10.0, adminId: 1).id
         end
         run_test!
       end
     end
   end
+  # rubocop:enable Metrics/BlockLength
 end
