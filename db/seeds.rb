a38bb753eadad7181a9b7247eeb6332dd6b98d0c
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-28 21:17:43 +0530

    Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,9 @@ user1 = User.create(username: 'umair_rao', firstName: 'Umair', lastName: 'Rao', 
 user2 = User.create(username: 'ali_rao', firstName: 'ali', lastName: 'rao', city: 'Karachi')
 
 # Create Items
-item1 = Item.create(admin: admin, name: 'BMW', image: 'https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80', description: 'Exquisite design, powerful performance, and unparalleled luxury redefine driving excellence.', city: 'Chicago', adminId: '1')
-item2 = Item.create(admin: admin, name: 'Bugatti', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvfuMwm1p6V4fTWCEb5irnaDB-EdXYJzjTA&usqp=CAU', description: 'Unrivaled speed, precision engineering, and opulent design redefine automotive luxury.', city: 'San Francisco', adminId: '1')
-item3 = Item.create(admin: admin, name: 'Honda', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSySr0V2R5ZFfVlTiI6S8IdtIBxX2QW32oFfA&usqp=CAU', description: 'Reliable, efficient, and versatile vehicles, delivering quality and innovation effortlessly.', city: 'Seattle', adminId: '1')
+item1 = Item.create(admin: admin, name: 'BMW', image: 'https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80', description: 'Exquisite design, powerful performance, and unparalleled luxury redefine driving excellence.', city: 'Chicago', financeFee: '20', purchaseFee: '20', totalAmount: '32', apr: '2',duration:'3', adminId: '1')
+item2 = Item.create(admin: admin, name: 'Bugatti', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvfuMwm1p6V4fTWCEb5irnaDB-EdXYJzjTA&usqp=CAU', description: 'Unrivaled speed, precision engineering, and opulent design redefine automotive luxury.', city: 'San Francisco', financeFee: '20', purchaseFee: '20', totalAmount: '32', apr: '2',duration:'3', adminId: '1')
+item3 = Item.create(admin: admin, name: 'Honda', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSySr0V2R5ZFfVlTiI6S8IdtIBxX2QW32oFfA&usqp=CAU', description: 'Reliable, efficient, and versatile vehicles, delivering quality and innovation effortlessly.', city: 'Seattle', financeFee: '20', purchaseFee: '20', totalAmount: '32', apr: '2',duration:'3', adminId: '1')
 
 # Create Reservations
 reservation1 = Reservation.create(customer: user2, reserveForUseDate: Date.today, city: 'Los Angeles', customer_id: '1', item_id: '2')
